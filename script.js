93d9a4f423f45a517ae7d2f7b74fcbab9b59f921
Khuzaima Yunus <118909350+khuziyunus@users.noreply.github.com>
2025-06-23 22:50:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll("nav a");
-  links.forEach(a => {
-    a.addEventListener("click", e => {
-      e.preventDefault();
-      document.querySelector(a.getAttribute("href"))
-        .scrollIntoView({ behavior: "smooth" });
-    });
-  });
-});
+
